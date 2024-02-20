77e6b6cc61b3a1384dc56af4beab7f9c6d23b1b9
MansiParsaniya <parsaniyamansi8141@gmail.com>
2024-02-20 10:03:31 +0530

    changes

--- connect.js
+++ connect.js
@@ -4,9 +4,9 @@ const connection = async () => {
     try {
 
         // MongoDB Connection URL
-        // await mongoose.connect('mongodb+srv://parsaniyamansi8141:parsaniyamansi8141@cluster0.9q5t3bu.mongodb.net/vijayho_api')
+        await mongoose.connect('mongodb+srv://parsaniyamansi8141:parsaniyamansi8141@cluster0.9q5t3bu.mongodb.net/vijayho_api')
         // await mongoose.connect('mongodb+srv://parsaniyamansi8141:parsaniyamansi8141@cluster0.9q5t3bu.mongodb.net/')
-        await mongoose.connect('mongodb://0.0.0.0:27017/vijayho_api')
+        // await mongoose.connect('mongodb://0.0.0.0:27017/vijayho_api')
         // mongodb+srv://<username>:<password>@<cluster-uri>/<database>?retryWrites=true&w=majority
         console.log("Mongo Database Connectioned ! ")
 
