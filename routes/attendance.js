8bbc356262d900b8aa0f49cb90392a0ee87be845
MansiParsaniya <parsaniyamansi8141@gmail.com>
2024-02-27 16:32:25 +0530

    changes

--- routes/attendance.js
+++ routes/attendance.js
@@ -6,7 +6,7 @@ const attendanceController = require('../controllers/attendanceController');
 router.post('/:enrollmentNumber/add-attendance', attendanceController.addAttendance)
 router.get('/getAttendance/:date', attendanceController.getAttendanceByDate)
 // router.patch('/:enrollmentNumber/:date', attendanceController.updateAttendance);
-router.patch('/update/:enrollmentNumber/:date', attendanceController.updateAttendance);
+router.patch('/attendance/:enrollmentNumber/:date', attendanceController.updateAttendance);
 
 // =========================================  Extra Work  ======================================================== 
 
